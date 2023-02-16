9c5bcc97f1d6be1de0b172fa1e1cb619f6573955
gungdikaebs <gungdika88@gmail.com>
2023-02-16 20:18:54 +0800

    Projectan Iseng

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Cick diluar Sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
